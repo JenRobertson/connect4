7001968a1f1c8725068366e7c7a59fea6bcc3f49
Jenny ☺ <jenny@jennyrobertson.net>
2020-06-20 20:42:28 +0100

    test using heroku

--- frontend.js
+++ frontend.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', (event) => {
-    var socket = io('http://localhost:3000');
+    // const socket = io('http://localhost:3000');
+    const socket = io('https://waving-walrus-connect4.herokuapp.com');
     const canvas = document.querySelector('canvas');
     const winsDiv = document.querySelector('#wins-div');
     const winsSpan = document.querySelector('#wins-span');
